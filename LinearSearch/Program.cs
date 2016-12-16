1bf1cfaa7deca15f62b814f9fac551af094f661b
Venkata Sreedhar <Venkata Sreedhar>
2016-12-15 17:12:01 -0800

    Code Completed and ready for testing

--- LinearSearch/Program.cs
+++ LinearSearch/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace LinearSearch
 {
@@ -10,6 +11,23 @@ namespace LinearSearch
     {
         static void Main(string[] args)
         {
+            int[] a = new int[10];
+            Console.WriteLine("\n Enter a set of 10 values into the Array:");
+            for(int i=0; i<a.Length; i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("\n Enter a number to search your Array through Linear Search:");
+            int x= Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == x)
+                {
+                    Console.WriteLine("\n Liniear Search is performed and we found the number at index:" + i);
+                    break;
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
